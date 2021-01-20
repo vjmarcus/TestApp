96f3e30925e14bac49b41daceedfe1eb83a150c5
vjmarcus <vjmarcus@list.ru>
2021-01-20 15:45:20 +0300

    Ready

--- app/src/main/java/com/example/testapp/SemaforActivity.kt
+++ app/src/main/java/com/example/testapp/SemaforActivity.kt
@@ -15,24 +15,44 @@ class SemaforActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySemaforBinding
     private var timer: Timer? = null
+    private var isRun : Boolean = false
+    private var counter = 0
+    private val imageList: ArrayList<Int> = arrayListOf(R.drawable.semafor_red,
+        R.drawable.semafor_yellow, R.drawable.semafor_green)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySemaforBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        val imageList: ArrayList<Int> = arrayListOf(R.drawable.semafor_green,
-            R.drawable.semafor_yellow, R.drawable.semafor_red)
-
-        binding.imageViewSemafor.setImageResource(R.drawable.semafor_green)
+        binding.imageViewSemafor.setImageResource(R.drawable.semafor_grey)
         binding.imageButtonStartStop.setOnClickListener { view:View ->
             Log.d(Constants.TAG, "onCreate: click!")
+            startStopTimer()
         }
     }
 
     private fun startStopTimer() {
+        if (isRun) {
+            timer?.cancel()
+            binding.imageViewSemafor.setImageResource(R.drawable.semafor_grey)
+            isRun = false
+        } else {
+            timer = Timer()
+            counter = 0
+            timer?.schedule(object : TimerTask() {
+                override fun run() {
+                    changeImage()
+                }
+            }, 0, 1000)
+            isRun = true
+        }
+    }
 
+    private fun changeImage() {
+        binding.imageViewSemafor.setImageResource(imageList[counter])
+        counter++
+        if (counter == 3) counter = 0
     }
 
 
